20d6d5c374c17040b6088c093565c397a5887669
raskal-dev <kalvinrasamyzo@gmail.com>
2024-07-09 22:44:24 +0300

    test in the clossaire

--- Main.py
+++ Main.py
@@ -1,1 +1,20 @@
-print("Hello World")
+import abc from ABC , abstractmethod
+
+class Animal(ABC):
+    @abstractmethod
+    def speak(self):
+        pass
+
+class Chien(Animal):
+    def speak(self):
+        return "Wouf Wouf"
+
+class Chat(Animal):
+    def speak(self):
+        return "Miaou Miaou"
+
+mon_chien = Chien()
+mon_chat = Chat()
+
+print(mon_chien.speak())
+print(mon_chat.speak())
